ee3c399ee5a48b3a9098e63a0fb5c14661ca15e6
Mukesh Maurya <mbmaurya.maurya@gmail.com>
2019-03-25 15:20:38 +0530

    Added Drop-in authentication solution from firebaseui

--- auth-config.js
+++ auth-config.js
@@ -23,6 +23,31 @@ privacyPolicyUrl: function() {
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+// ui.start('#firebaseui-auth-container', uiConfig);
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        // Google provider must be enabled in Firebase Console to support one-tap
+        // sign-up.
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // Required to enable this provider in one-tap sign-up.
+        authMethod: 'https://accounts.google.com',
+        // Required to enable ID token credentials for this provider.
+        // This can be obtained from the Credentials page of the Google APIs
+        // console.
+        clientId: '209135031424-ajnjdgdnmiec3bl6s1n5n35fbg8aogj7.apps.googleusercontent.com'
+      },
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Required to enable one-tap sign-up credential helper.
+    credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+  });
+  // Auto sign-in for returning users is enabled by default except when prompt is
+  // not 'none' in the Google provider custom parameters. To manually disable:
+  ui.disableAutoSignIn();
 
 
